8113f6d0b08b26bedf074e7134edd901014e1dd7
Proteus <pro@proteanx.com>
2024-05-06 22:52:36 -0400

    add mint&burn events

--- contracts/openmint.sol
+++ contracts/openmint.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// @dev: This contract is a simple token minting contract that allows anyone to mint tokens.
-// It is intended to mimmick the behavior of minting brc20 or runes tokens on bitcoin
+// @dev: This contract is a simple token contract that allows anyone to mint tokens.
+// It is intended to mimmick the behavior of minting brc20 or runes tokens on bitcoin.
+// Additional features are the ability to burn tokens. 
 contract OpenMint is ERC20 {
     uint256 public maxSupply;
     uint256 public mintAmount;
@@ -28,6 +29,9 @@ contract OpenMint is ERC20 {
         maxSupply = maxMints * mintAmount;
     }
 
+    event Minted(address indexed minter);
+    event Burned(address indexed burner, uint256 amount);
+
     function mintsRemaining() public view returns (uint256) {
         if (block.number > endBlock) {
             return 0;
@@ -40,16 +44,22 @@ contract OpenMint is ERC20 {
         require(totalMints < maxMints, "Maximum mints reached");
         _mint(msg.sender, mintAmount);
         totalMints += 1;
+
+        emit Minted(msg.sender);
     }
 
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
         totalBurns += amount;
+
+        emit Burned(msg.sender, amount);
     }
     
     function burnFrom(address from, uint256 amount) public {
         _burn(from, amount);
         totalBurns += amount;
+
+        emit Burned(from, amount);
     }
 }
 

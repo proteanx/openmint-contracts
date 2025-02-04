06dcf3645ba9580887693145456446b2556a3e34
Proteus <pro@proteanx.com>
2025-02-03 22:04:40 -0500

    add max mints per address

--- contracts/openmint.sol
+++ contracts/openmint.sol
@@ -15,21 +15,27 @@ contract OpenMint is ERC20 {
     uint256 public startBlock;
     uint256 public endBlock;
     uint256 public maxMints;
+    uint256 public maxPerAddress;
     
+    mapping(address => uint256) public mintsPerAddress;
     mapping(address => uint256) public lastMintBlock;
     
     constructor(
         uint256 _maxMints,
         uint256 _mintAmount,
         uint256 _startBlock,
-        uint256 _endBlock
+        uint256 _endBlock,
+        uint256 _maxPerAddress
     ) ERC20("Open Mint", "OMINT") {
         maxMints = _maxMints;
         mintAmount = _mintAmount;
         require(_endBlock > _startBlock, "End block must be greater than start block");
         require(_endBlock > block.number, "End block must be in the future");
+        require(_maxPerAddress > 0, "Max per address must be greater than 0");
+        require(_maxPerAddress <= _maxMints, "Max per address cannot exceed total max mints");
         startBlock = _startBlock;
         endBlock = _endBlock;
+        maxPerAddress = _maxPerAddress;
         maxSupply = maxMints * mintAmount;
     }
 
@@ -47,10 +53,12 @@ contract OpenMint is ERC20 {
         require(block.number >= startBlock && block.number <= endBlock, "Mint not in progress");
         require(totalMints < maxMints, "Maximum mints reached");
         require(block.number > lastMintBlock[msg.sender] + 1, "Must wait 1 block between mints");
+        require(mintsPerAddress[msg.sender] < maxPerAddress, "Maximum mints per address reached");
         
         _mint(msg.sender, mintAmount);
         totalMints += 1;
         lastMintBlock[msg.sender] = block.number;
+        mintsPerAddress[msg.sender] += 1;
 
         emit Minted(msg.sender);
     }
